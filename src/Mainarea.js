dbd7ec4ee6fbe3e6d3f1031bb17a8592f8d6ba04
francisseth23 <francisseth23@gmail.com>
2021-05-03 13:36:08 +1000

    Mainarea from francis

---
+++ src/Mainarea.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const MainArea = (props)=>{
+    return(
+        <div>
+            <h3>This is my MainArea of the Page</h3>
+        </div>
+    )
+}
+
+export default MainArea;
