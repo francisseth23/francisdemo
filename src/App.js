bf19d8b297b9c4eeaeffe1035e1b4550158e8562
manumitsacademy <manumits.info@gmail.com>
2021-05-03 08:42:58 +0530

    footercomponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       <h1>Francis</h1>
       <h2>Demo Update3</h2>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Footer = (props)=>{
+    return(
+        <div>
+            <h3>This is my Footer</h3>
+        </div>
+    )
+}
+export default Footer;
